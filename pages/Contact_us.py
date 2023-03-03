463b6e5d8894c39a13e985788bdd5000f912fb8b
emahon <emahon@druidsoftware.com>
2023-03-03 11:44:53 +0000

    Send email, update picture

--- pages/Contact_us.py
+++ pages/Contact_us.py
@@ -1,3 +1,9 @@
 import streamlit as st
 
 st.header('Contact me')
+
+with st.form(key='my_form'):
+    user_email = st.text_input('Your email address')
+    message = st.text_area('Your message here')
+    button = st.form_submit_button('Submit')
+    if button:
